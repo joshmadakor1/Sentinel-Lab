cab6f8822f08cec0421a44508b1e66b2f178d591
joshmadakor1 <josh.madakor@Gmail.com>
2021-10-17 08:28:47 -0700

    Create Custom_Security_Log_Exporter.ps1
    Initial Commit with code

---
+++ Custom_Security_Log_Exporter.ps1
@@ -0,0 +1,87 @@
+﻿# Get API key from here: https://ipgeolocation.io/
+$API_KEY      = "e42fe5012e114f51ad939f6c7ce53be5"
+$LOGFILE_NAME = "failed_rdp.log"
+$LOGFILE_PATH = "C:\ProgramData\$($LOGFILE_NAME)"
+
+# This filter will be used to filter failed RDP events from Windows Event Viewer
+$XMLFilter = @'
+<QueryList> 
+   <Query Id="0" Path="Security">
+         <Select Path="Security">
+              *[System[(EventID='4625')]]
+          </Select>
+    </Query>
+</QueryList> 
+'@
+
+# This block of code will create the log file if it doesn't already exist
+if ((Test-Path $LOGFILE_PATH) -eq $false) {
+    New-Item -ItemType File -Path $LOGFILE_PATH
+}
+
+# Infinite Loop that keeps checking the Event Viewer logs.
+while ($true)
+{
+    
+    Start-Sleep -Seconds 1
+    # This retrieves events from Windows EVent Viewer based on the filter
+    $events = Get-WinEvent -FilterXml $XMLFilter -ErrorAction SilentlyContinue
+    if ($Error) {
+        Write-Host "No Failed Logons found. Re-run script when a login has failed."
+    }
+
+    # Step through each event collected, get geolocation
+    #    for the IP Address, and add new events to the custom log
+    foreach ($event in $events) {
+
+
+        # $event.properties[19] is the source IP address of the failed logon
+        # This if-statement will proceed if the IP address exists (>= 5 is arbitrary, just saying if it's not empty)
+        if ($event.properties[19].Value.Length -ge 5) {
+
+            # Pick out fields from the event. These will be inserted into our new custom log
+            $timestamp = $event.TimeCreated
+            $timestamp = "$($timestamp.Year)-$($timestamp.Month)-$($timestamp.Day) $($timestamp.Hour):$($timestamp.Minute):$($timestamp.Second)"
+            $eventId = $event.Id
+            $destinationHost = $event.MachineName# Workstation Name (Destination)
+            $username = $event.properties[5].Value # Account Name (Attempted Logon)
+            $sourceHost = $event.properties[11].Value # Workstation Name (Source)
+            $sourceIp = $event.properties[19].Value # IP Address
+        
+
+            # Get the current contents of the Log file!
+            $log_contents = Get-Content -Path $LOGFILE_PATH
+
+            # Do not write to the log file if the log already exists.
+            if (-Not ($log_contents -match "$($timestamp)") -or ($log_contents.Length -eq 0)) {
+            
+                # Announce the gathering of geolocation data and pause for a second as to not rate-limit the API
+                #Write-Host "Getting Latitude and Longitude from IP Address and writing to log" -ForegroundColor Yellow -BackgroundColor Black
+                Start-Sleep -Seconds 1
+
+                # Make web request to the geolocation API
+                # For more info: https://ipgeolocation.io/documentation/ip-geolocation-api.html
+                $API_ENDPOINT = "https://api.ipgeolocation.io/ipgeo?apiKey=$($API_KEY)&ip=$($sourceIp)"
+                $response = Invoke-WebRequest -UseBasicParsing -Uri $API_ENDPOINT
+
+                # Pull Data from the API response, and store them in variables
+                $responseData = $response.Content | ConvertFrom-Json
+                $latitude = $responseData.latitude
+                $longitude = $responseData.longitude
+                $state_prov = $responseData.state_prov
+                $country = $responseData.country_name
+
+                # Write all gathered data to the custom log file. It will look something like this:
+                #
+                #    2021-10-14 16:56:51|4625|randomname123|ADMINISTRATOR|44.192.33.238|47.91542|-120.60306|Washington|United States|44.192.33.238: Washington, United States
+                #
+                "$($timestamp)|$($eventId)|$($destinationHost)|$($username)|$($sourceIp)|$($latitude)|$($longitude)|$($state_prov)|$($country)|$($sourceIp): $($state_prov), $($country)" | Out-File $LOGFILE_PATH -Append -Encoding utf8
+                Write-Host -BackgroundColor Black -ForegroundColor Green "Writing log: $($timestamp): LOGIN FAILURE: $($username), ($sourceIp): $($state_prov), $($country)"
+            }
+            else {
+                # Entry already exists in custom log file. Do nothing, optionally, remove the # from the line below for output
+                # Write-Host "Event already exists in the custom log. Skipping." -ForegroundColor Gray -BackgroundColor Black
+            }
+        }
+    }
+}
